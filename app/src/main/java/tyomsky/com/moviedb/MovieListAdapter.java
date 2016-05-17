10ba7f27a3cef0ebe7d9394f643604c4b00d03c2
Artem Degtyaryov <tyomsky@gmail.com>
2016-05-18 01:29:14 +0300

    add picasso and basic images fetching

--- app/src/main/java/tyomsky/com/moviedb/MovieListAdapter.java
+++ app/src/main/java/tyomsky/com/moviedb/MovieListAdapter.java
@@ -1,19 +1,29 @@
 package tyomsky.com.moviedb;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import tyomsky.com.moviedb.model.Movie;
 
 public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>{
 
+    private static final String BASE_IMAGE_URI = "http://image.tmdb.org/t/p/";
+    private static final String IMAGE_SIZE_PATH = "w185";
+    private static final String TAG = "MovieListAdapter";
     private Context context;
     private List<Movie> movies = new ArrayList<>();
 
@@ -25,7 +35,7 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        return new MovieViewHolder(inflater.inflate(android.R.layout.simple_list_item_1, parent, false));
+        return new MovieViewHolder(inflater.inflate(R.layout.movie_item, parent, false));
     }
 
     @Override
@@ -35,7 +45,7 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
-        holder.bindText(movies.get(position));
+        holder.bindMovie(movies.get(position));
     }
 
     public void setMovies(List<Movie> movies) {
@@ -45,15 +55,26 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
 
     class MovieViewHolder extends RecyclerView.ViewHolder{
 
-        TextView textView;
+        @Bind(R.id.movie_item_image_view)
+        ImageView imageView;
 
         public MovieViewHolder(View view) {
             super(view);
-            textView = (TextView) view.findViewById(android.R.id.text1);
+            ButterKnife.bind(this, view);
         }
 
-        public void bindText(Movie movie) {
-            textView.setText(movie.getTitle());
+        public void bindMovie(Movie movie) {
+            String moviePoster = movie.getPosterPath();
+            Uri imageUri = Uri.parse(BASE_IMAGE_URI).buildUpon()
+                    .appendPath(IMAGE_SIZE_PATH)
+                    .appendEncodedPath(moviePoster)
+                    .build();
+            Log.d(TAG, imageUri.toString());
+
+            Picasso.with(context)
+                    .load(imageUri)
+                    .error(android.R.drawable.ic_dialog_alert)
+                    .into(imageView);
         }
     }
 
