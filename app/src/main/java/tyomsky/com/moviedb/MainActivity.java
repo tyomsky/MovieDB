2a360f60025d619b731a02fd51679fee664581c4
tyomsky <tyomsky@gmail.com>
2016-05-18 15:38:51 +0300

    add sort by spinner

--- app/src/main/java/tyomsky/com/moviedb/MainActivity.java
+++ app/src/main/java/tyomsky/com/moviedb/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,8 +31,8 @@ import tyomsky.com.moviedb.model.Movie;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TOP_RATED = "vote_average.desc";
-    private static final String POPULAR = "popularity.desc";
+    private static final String TOP_RATED_METHOD = "vote_average.desc";
+    private static final String POPULAR_METHOD = "popularity.desc";
 
     @Bind(R.id.recycler_view)
     RecyclerView recyclerView;
@@ -38,15 +44,47 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         recyclerView.setAdapter(new MovieListAdapter(this, new ArrayList<Movie>()));
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        new LoadingTask().execute();
+        updateMovies("popularity");
     }
 
-    private class LoadingTask extends AsyncTask<Void, Void, List<Movie>> {
+    private void updateMovies(String sortBy) {
+        new LoadingTask().execute(sortBy);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem item = menu.findItem(R.id.action_sort_by);
+        Spinner spinner = (Spinner) item.getActionView();
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.sort_by_entries,
+                android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String sortBy = getResources().getStringArray(R.array.sort_by_entry_values)[position];
+                updateMovies(sortBy);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
+        return true;
+    }
+
+    private class LoadingTask extends AsyncTask<String, Void, List<Movie>> {
 
         private static final String TAG = "LoadingTask";
 
         @Override
-        protected List<Movie> doInBackground(Void... params) {
+        protected List<Movie> doInBackground(String... params) {
+            String sortBy = params[0];
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
                     .addInterceptor(new Interceptor() {
                         @Override
@@ -67,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
             MovieDBService service = retrofit.create(MovieDBService.class);
             List<Movie> movies = null;
             try {
-                movies = service.getMovies(TOP_RATED).execute().body().getResults();
+                movies = service.getMovies("popular".equals(sortBy) ? POPULAR_METHOD : TOP_RATED_METHOD)
+                        .execute().body().getResults();
             } catch (IOException e) {
                 Log.e(TAG, "Cant get movies", e);
             }
--- app/src/main/java/tyomsky/com/moviedb/MovieListAdapter.java
+++ app/src/main/java/tyomsky/com/moviedb/MovieListAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -21,9 +20,9 @@ import tyomsky.com.moviedb.model.Movie;
 
 public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>{
 
+    private static final String TAG = "MovieListAdapter";
     private static final String BASE_IMAGE_URI = "http://image.tmdb.org/t/p/";
     private static final String IMAGE_SIZE_PATH = "w185";
-    private static final String TAG = "MovieListAdapter";
     private Context context;
     private List<Movie> movies = new ArrayList<>();
 
